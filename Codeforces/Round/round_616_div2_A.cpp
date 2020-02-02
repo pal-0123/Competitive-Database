036d85b50fe2d8c299c62e0c904c1684e971a06f
pal-0123 <akinchannr@hotmail.com>
2020-02-02 19:33:44 +0530

    Codeforces

---
+++ Codeforces/Round/round_616_div2_A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+signed main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+	#endif
+
+ 
+
+	return 0;
+}
